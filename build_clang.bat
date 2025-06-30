8118e2643e7bf9413b00f86c6aac059f038abf06
brinkqiang <brink.qiang@gmail.com>
2025-06-30 17:01:22 +0800

    add

--- build_clang.bat
+++ build_clang.bat
@@ -5,6 +5,8 @@ cd build
 
 cmake -A x64 -DCMAKE_BUILD_TYPE=relwithdebinfo -T ClangCL ..
 cmake --build . --config relwithdebinfo -- /m:%NUMBER_OF_PROCESSORS%
+echo "cd build && ctest -C relwithdebinfo --output-on-failure --output-junit report.xml && cd .."
+
 cd ..
 
 rem pause
--- build_clang.sh
+++ build_clang.sh
@@ -6,6 +6,7 @@ cd build
 
 cmake -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_BUILD_TYPE=relwithdebinfo ..
 cmake --build . --config relwithdebinfo -- -j$(nproc)
+echo "cd build && ctest -C relwithdebinfo --output-on-failure --output-junit report.xml && cd .."
 
 cd ..
 
