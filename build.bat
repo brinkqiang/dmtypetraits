a422ef18f5d76d8616feb5fc707a7ed80b829d01
brinkqiang <brink.qiang@gmail.com>
2025-06-30 14:18:14 +0800

    add

--- build.bat
+++ build.bat
@@ -1,10 +1,11 @@
 
-rmdir /S /Q build
-mkdir build
-pushd build
 
+rmdir /S /Q build 2> nul
+mkdir build
+cd build
 cmake -A x64 -DCMAKE_BUILD_TYPE=relwithdebinfo ..
 cmake --build . --config relwithdebinfo -- /m:%NUMBER_OF_PROCESSORS%
-popd
+echo "cd build && ctest -C relwithdebinfo --output-on-failure --output-junit report.xml && cd .."
+cd ..
 
 rem pause
--- build.sh
+++ build.sh
@@ -2,11 +2,15 @@
 
 rm -rf build
 mkdir -p build
+
+# pushd build
 cd build
 
-cmake -DCMAKE_BUILD_TYPE=relwithdebinfo ..
+# -DCMAKE_EXPORT_COMPILE_COMMANDS=ON option is implemented only by Makefile Generators and Ninja Generators.
+cmake -DCMAKE_BUILD_TYPE=relwithdebinfo -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
 cmake --build . --config relwithdebinfo -- -j$(nproc)
-
+echo "cd build && ctest -C relwithdebinfo --output-on-failure --output-junit report.xml && cd .."
+# popd
 cd ..
 
 # echo continue && read -n 1
