928015a2f96d1e4fbdb384c7e9534bfa2fd934c8
brinkqiang <brink.qiang@gmail.com>
2025-09-07 14:37:43 +0800

    add

--- examples/dmtypetraits_md5/dmtypetraits_md5.cpp
+++ examples/dmtypetraits_md5/dmtypetraits_md5.cpp
@@ -4,15 +4,13 @@
 
 
 int main() {
-    std::cout << "=== 编译时平台检测 ===" << std::endl;
 
-    dm::platform::PrintCurrentPlatformInfo();
-
-    std::cout << "\n=== 基于静态函数的平台类型 ===" << std::endl;
     using HashPlatform = dm::platform::CurrentPlatform;
     using HashTraits = dm::platform::PlatformHashTraits<HashPlatform>;
-    std::cout << "哈希平台名称: " << HashTraits::name() << std::endl;
-    std::cout << "哈希值: 0x" << std::hex << HashPlatform::hash_value << std::endl;
 
+    std::cout << "平台名称: " << HashTraits::name() << std::endl;
+    std::cout << "是否Unix-like: " << (HashTraits::is_unix_like() ? "是" : "否") << std::endl;
+    std::cout << "文件分隔符: " << HashTraits::file_separator() << std::endl;
+    std::cout << "平台哈希值: 0x" << std::hex << HashPlatform::hash_value << std::endl;
     return 0;
 }
